1bb675a42bb079709a56795b7e004d99fd114f2d
Maxwell Falls <fallsmaxwell@gmail.com>
2022-07-30 21:39:34 +0100

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,11 +6,17 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>                     __                             ___       __  
-   ____  ____  _____/ /__________  ____ ___  ____  / (_)___  / /__
-  / __ \\/ __ \\/ ___/ __/ ___/ __ \\/ __ '__ \\/ __ \\/ / / __ \\/ //_/
- / / / / /_/ (__  ) /_/ /  / /_/ / / / / / / /_/ / / / / / / ,&lt;   
-/_/ /_/\\____/____/\\__/_/   \\____/_/ /_/ /_/\\____/_/_/_/ /_/_/|_|  
+<div class="custom-text-header"> <pre>
+                                           d8,                  
+                                          `8P    d8P            
+                                              d888888P          
+ .d888b, d8888b  88bd88b d8888b  88bd88b   88b  ?88'  ?88   d8P 
+ ?8b,   d8b_,dP  88P'  `d8b_,dP  88P' ?8b  88P  88P   d88   88  
+   `?8b 88b     d88     88b     d88   88P d88   88b   ?8(  d88  
+`?888P' `?888P'd88'     `?888P'd88'   88bd88'   `?8b  `?88P'?8b 
+                                                             )88
+                                                            ,d8P
+                                                         `?888P'
                                                                  </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
